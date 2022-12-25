66cf1681f0c0d8a1ef43e9811fb956f30899e027
Piotr-reader <piotr_partsianko@inbox.ru>
2022-12-25 22:14:17 +0300

    feat: implement task filter

--- filter/Filter.js
+++ filter/Filter.js
@@ -2,47 +2,53 @@ const Filter = React.createClass({
   displayName: "Filter",
   propTypes: {
     string: React.PropTypes.array.isRequired,
-    defVal: React.PropTypes.bool.isRequired,
-    defvaltext: React.PropTypes.string.isRequired,
   },
   getInitialState: function () {
     return {
-      cnt: this.props.string,
-      checkBoxVal: this.props.defVal,
-      textVal: this.props.defvaltext,
+      text: this.props.string.slice(),
+      isSort: false,
+      filter: "",
     };
   },
   sortArr: function (e) {
-    if (e.target.checked) {
-      this.state.checkBoxVal = true;
-      this.setState({ cnt: this.state.cnt.sort() });
-    } else {
-      this.state.checkBoxVal = false;
-      this.setState({ cnt: this.state.cnt });
-    }
+    this.setState({ isSort: e.target.checked }, this.textAreaChange);
   },
 
-  validArr: function (EO) {
-    let newStr = [];
-    this.props.string.map((item) => {
-      var re = new RegExp(EO.target.value);
-      if (item.match(re)) {
-        newStr.push(item);
-      }
-    });
-    this.setState({ cnt: newStr }, this.sortArr);
+  filterArr: function (EO) {
+    this.setState({ filter: EO.target.value }, this.textAreaChange);
   },
+
   resetFn: function () {
-    this.setState({ checkBoxVal: false, textVal: "" });
+    this.setState({ isSort: false, filter: "", text: this.props.string.slice() });
   },
-  textAreaChange: function () {},
+  textAreaChange: function () {
+    if (this.state.filter) {
+      let newStr = [];
+      this.props.string.slice().map((item) => {
+        if (item.match(new RegExp(this.state.filter))) {
+          newStr.push(item);
+        }
+      });
+      this.setState({ text: newStr });
+    }
+    if (this.state.filter === "") {
+      this.setState({ text: this.props.string.slice() });
+    }
+
+    setTimeout(() => {
+      if (this.state.isSort) {
+        this.setState({ text: this.state.text.sort() });
+      }
+    }, 0);
+  },
+  Fn: function () {},
   render: function () {
     return React.DOM.div(
       { className: "Filter" },
-      React.DOM.input({ className: "checkbox", type: "checkbox", defaultChecked: this.state.checkBoxVal, onClick: this.sortArr }),
-      React.DOM.input({ className: "textInput", type: "text", defaultValue: this.state.textVal, onChange: this.validArr }),
+      React.DOM.input({ className: "checkbox", type: "checkbox", checked: this.state.isSort, onClick: this.sortArr }),
+      React.DOM.input({ className: "textInput", type: "text", value: this.state.filter, onChange: this.filterArr }),
       React.DOM.input({ type: "button", defaultValue: "сброс", onClick: this.resetFn }),
-      React.DOM.div(null, React.DOM.textarea({ className: "text", value: this.state.cnt.join("\n"), onChange: this.textAreaChange }))
+      React.DOM.div(null, React.DOM.textarea({ className: "text", value: this.state.text.join("\n"), onChange: this.Fn }))
     );
   },
 });
