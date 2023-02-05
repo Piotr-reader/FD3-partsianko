6cfaed39cac7ab960dcf3eeaa2fa41aeb90f2c5c
Piotr-reader <piotr_partsianko@inbox.ru>
2023-02-05 19:12:15 +0300

    feat:finish task FilterHook

--- FilterHook/components/Controls .js
+++ FilterHook/components/Controls .js
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 
 import "./Controls.css";
 const Controls = (props) => {
+  const [filter, setFilter] = useState("");
+  const [isSort, setIsSort] = useState(false);
+  const checkboxRef = useRef(isSort);
+  const filterRef = useRef(filter);
+  useEffect(() => {
+    let newStr = props.string.slice();
+    if (filter) {
+      newStr = newStr.filter((word) => word.includes(filter));
+    }
+    if (isSort) {
+      newStr = newStr.sort();
+    }
+    props.cbSetText(newStr);
+  }, [isSort, filter]);
 
+  const resetFn = () => {
+    setFilter("");
+    setIsSort(false);
+  };
   return (
-    <input
-      className={props.controlOptions.class}
-      type={props.controlOptions.type}
-      value={props.controlOptions.value}
-      checked={props.controlOptions.checked}
-      defaultValue={props.controlOptions.defaultValue}
-      onClick={props.controlOptions.onClick}
-      onChange={props.controlOptions.onChange}
-      ref={props.controlOptions.ref}
-    />
+    <div>
+      <input className="checkbox" ref={checkboxRef} type="checkbox" checked={isSort} onChange={() => setIsSort(checkboxRef.current.checked)} />
+      <input className="filter" ref={filterRef} type="text" value={filter} onChange={() => setFilter(filterRef.current.value)} />
+      <input className="button" type="button" defaultValue="Сброс" onClick={resetFn} />
+    </div>
   );
 };
 Controls.propTypes = {
-  controlOptions: PropTypes.object,
+  string: PropTypes.array.isRequired,
+  cbSetText: PropTypes.func.isRequired,
 };
 
 export default Controls;
--- FilterHook/components/Filter.js
+++ FilterHook/components/Filter.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 import "./Filter.css";
@@ -7,35 +7,11 @@ import Controls from "./Controls ";
 
 const Filter = (props) => {
   const [text, setText] = useState(props.string.slice());
-  const [filter, setFilter] = useState("");
-  const [isSort, setIsSort] = useState(false);
-  const checkboxRef= useRef(isSort);
-  const filterRef= useRef(filter);
 
-  useEffect(() => {
-    let newStr = props.string.filter((word) => word.includes(filter));
-    if (isSort) {
-      newStr = newStr.sort();
-    }
-    setText(newStr);
-  }, [isSort, filter]);
-
-  const resetFn = () => {
-    setFilter("");
-    setIsSort(false);
-  };
-  let typeBtn = [
-    { class: "checkbox", ref:checkboxRef, type: "checkbox", checked: isSort, onChange: () => setIsSort(checkboxRef.current.checked) },
-    { class: "filter",ref:filterRef, type: "text", value: filter, onChange: () => setFilter(filterRef.current.value) },
-    { class: "button", type: "button", defaultValue: "Сброс", onClick: resetFn },
-  ];
-  let btnSettings = typeBtn.map((btn, index) => {
-    return <Controls key={index} controlOptions={btn} />;
-  });
   return (
     <div className="Filter">
       <div className="btns">
-      {btnSettings}
+        <Controls string={props.string} cbSetText={setText} />
       </div>
       <List string={text} />
     </div>
